1dd75a5b69627b71e07735e79064aa321664758d
Egor Kalinkin <ekgrenader98@mail.ru>
2024-02-15 12:27:52 +0300

    Task 1 in Homework 6 is done

---
+++ 6. Processors and macros/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+int main()
+{
+    setlocale(0, "");
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки\n";
+#elif MODE == 1
+#define add(x, y) (x) + (y)
+    int x = 0, y = 0;
+    
+    std::cout << "Работаю в боевом режиме\n";
+    std::cout << "Введите число 1: ";
+    std::cin >> x;
+    std::cout << "Введите число 2: ";
+    std::cin >> y;
+    std::cout << "Результат сложения: " << add(x, y);
+#else 
+    std::cout << "Неизвестный режим. Завершение работы";
+#endif
+    
+    return 0;
+}
