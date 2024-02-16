eb9e9ddff0675fd4a1fb193e4919651cfcbcc718
Egor Kalinkin <ekgrenader98@mail.ru>
2024-02-16 17:06:22 +0300

    Task 2 editted

--- 6. Processors and macros/Task 1/Task 1/Task 1.cpp
+++ 6. Processors and macros/Task 1/Task 1/Task 1.cpp
@@ -1,16 +1,22 @@
 ﻿#include <iostream>
 
-#define MODE 1
+#define MODE 1 
 
 int main()
 {
     setlocale(0, "");
+
+#ifndef MODE
+#error Необходимо определить константу MODE
+#endif
+
+#ifdef MODE
 #if MODE == 0
     std::cout << "Работаю в режиме тренировки\n";
 #elif MODE == 1
 #define add(x, y) (x) + (y)
     int x = 0, y = 0;
-    
+
     std::cout << "Работаю в боевом режиме\n";
     std::cout << "Введите число 1: ";
     std::cin >> x;
@@ -20,6 +26,5 @@ int main()
 #else 
     std::cout << "Неизвестный режим. Завершение работы";
 #endif
-    
-    return 0;
+#endif
 }
