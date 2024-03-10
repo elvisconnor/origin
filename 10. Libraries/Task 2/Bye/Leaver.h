89de22c011050d1f1ab393309c3dd2fe0b5fc045
Egor Kalinkin <ekgrenader98@mail.ru>
2024-03-10 19:11:30 +0300

    Task 2 in Libraries completed

---
+++ 10. Libraries/Task 2/Bye/Leaver.h
@@ -0,0 +1,14 @@
+﻿#include <string>
+#pragma once
+#ifdef DYNAMICLIBRARY_EXPORTS
+#define DYNAMICLIBRARY_API __declspec(dllexport)
+#else 
+#define DYNAMICLIBRARY_API __declspec(dllimport)
+#endif 
+namespace DynamicLibrary
+{
+	 class Leaver {
+	public:
+		DYNAMICLIBRARY_API void leave(std::string name);
+	};
+}
