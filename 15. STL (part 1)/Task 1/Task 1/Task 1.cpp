770fe494ff55b6733f24761f5bf813707f8d8155
Egor Kalinkin <ekgrenader98@mail.ru>
2024-05-19 13:29:07 +0300

    Homework 15 is done

---
+++ 15. STL (part 1)/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+int main() {
+
+    std::string phrase = "Hello world!";
+    std::map<char, int> charFrequency;
+
+    for (char c : phrase) {
+        if (charFrequency.find(c) != charFrequency.end()) {
+            charFrequency[c] += 1;
+        }
+        else {
+            charFrequency[c] = 1;
+        }
+    }
+
+    std::map<int, char, std::greater<int>> sortedCharFrequency;
+
+    for (auto const& pair : charFrequency) {
+        sortedCharFrequency[pair.second] = pair.first;
+    }
+
+    for (auto const& pair : sortedCharFrequency) {
+        std::cout << pair.second << ": " << pair.first << std::endl;
+    }
+
+    for (auto const& pair : charFrequency) {
+        if (pair.second == 1 &&
+            sortedCharFrequency.find(pair.first) == sortedCharFrequency.end()) {
+            std::cout << pair.first << ": " << pair.second << std::endl;
+        }
+    }
+
+    return 0;
+}
---
+++ 15. STL (part 1)/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+int main() {
+    setlocale(0, "");
+    int count = 0;
+    std::cout << "Введите количество элементов: ";
+    std::cin >> count;
+
+    std::set<int> set;
+
+    for (int i = 0; i < count; i++) {
+        std::cout << "Введите элемент № " << i + 1 << ": ";
+        int num;
+        std::cin >> num;
+        set.insert(num);
+    }
+
+    std::vector<int> vec(set.begin(), set.end());
+
+    std::sort(vec.begin(), vec.end(), std::greater<int>());
+
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << "\n";
+    }
+
+    return 0;
+}
