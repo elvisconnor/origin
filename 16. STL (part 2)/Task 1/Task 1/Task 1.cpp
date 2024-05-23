6306476d65e8714c4655cf743d8c8d5fd741675c
Egor Kalinkin <ekgrenader98@mail.ru>
2024-05-23 19:54:11 +0300

    Homework 16 is done

---
+++ 16. STL (part 2)/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+auto func(std::vector<int>& vec) {
+    std::set<int> s;
+    for (int i = 0; i < vec.size(); i++) {
+        s.insert(vec[i]);
+    }
+    return s;
+}
+
+int main() {
+    std::vector<int> number = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::vector<int>::iterator i = number.begin();
+    while (i != number.end()) {
+        std::cout << *i << "\t";
+        i++;
+    };
+    std::cout << "\n" << "\n";
+
+    auto s = func(number);
+
+    for (auto i : s) {
+        std::cout << i << "\t";
+    }
+
+    return 0;
+}
---
+++ 16. STL (part 2)/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template <typename T>
+void print_container(const T& container) {
+    bool first = true;
+    for (const auto& element : *container) {
+        if (!first) {
+            std::cout << ", ";
+        }
+        else {
+            first = false;
+        }
+        std::cout << element;
+    }
+}
+
+int main() {
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(&test_set);
+    std::cout << "\n";
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(&test_list);
+    std::cout << "\n";
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(&test_vector);
+    std::cout << "\n";
+
+    return 0;
+}
