631744516628efc6541d6b52c00631ca7b5390b1
Egor Kalinkin <ekgrenader98@mail.ru>
2024-05-28 20:55:16 +0300

    Homework 17 is done

---
+++ 17. Smart pointers/Task 3/Task 3/Task 3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+template <typename T>
+class my_unique_ptr {
+private:
+    T* ptr;
+public:
+    my_unique_ptr(T* ptr) {
+        this->ptr = ptr;
+    };
+
+    my_unique_ptr& operator =(const my_unique_ptr& ptr) = delete;
+
+    my_unique_ptr(const my_unique_ptr& ptr) = delete;
+
+    T& operator *() {
+        return *ptr;
+    }
+
+    T* release() {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+
+    ~my_unique_ptr() {
+        delete[] ptr;
+    };
+};
+
+int main() {
+    int a = 5;
+    int* ptr = &a;
+
+    my_unique_ptr<int> ptr1(ptr);
+    std::cout << *ptr1 << std::endl;
+    std::cout << ptr1.release() << std::endl;
+
+    return 0;
+}
